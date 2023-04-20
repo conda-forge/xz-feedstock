6bb0de35e67a120a3403f41c686b06a577434f5c
Isuru Fernando <isuruf@gmail.com>
2023-04-19 20:08:41 -0500

    DLLs

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -17,6 +17,7 @@ cmake -GNinja ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_C_USE_RESPONSE_FILE_FOR_OBJECTS:BOOL=FALSE ^
       -DCMAKE_BUILD_TYPE=Release ^
+      -DBUILD_SHARED_LIBS=ON ^
       --debug-trycompile ^
       .
 ninja
