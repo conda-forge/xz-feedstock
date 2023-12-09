f5383dc385ee77ef03c772cdad677a7f3bcc9f88
fhoehle <fhoehle@users.noreply.github.com>
2023-12-09 18:24:01 +0100

    debug

--- recipe/install.bat
+++ recipe/install.bat
@@ -1,5 +1,31 @@
+@echo on
+
+echo "first output"
+
+if  %vc% LEQ 9 set MSVC_VER=1500
+if  %vc% GTR 9 set MSVC_VER=1900
+
+mkdir build
+if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
+
+cmake -G "Ninja" ^
+      "%CMAKE_ARGS%" ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DMSVC_VERSION="%MSVC_VER%" ^
+      -DCMAKE_C_USE_RESPONSE_FILE_FOR_OBJECTS:BOOL=FALSE ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DBUILD_SHARED_LIBS=ON ^
+      "%SRC_DIR%"
+
+if errorlevel 1 exit /b 1
+
+cmake --build . -j %CPU_COUNT% --verbose --config Release
+if errorlevel 1 exit /b 1
+
 
 cmake --build . --config Release --target install
 if errorlevel 1 exit /b 1
