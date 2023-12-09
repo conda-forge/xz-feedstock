a4ceafc9a60192c54cd8ee70e383210b008bc421
fhoehle <fhoehle@users.noreply.github.com>
2023-12-09 16:42:46 +0100

    add MSVC version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,8 +13,17 @@ if "%ARCH%" == "64" (
 :: if "%vc%" == "9" goto vc9_build
 :: goto vc14_build
 
+
+if  %vc% LEQ 9 set MSVC_VER=1500
+if  %vc% GTR 9 set MSVC_VER=1900
+
+if  %vc% LEQ 9 set MSVC_TS_VER=90
+if  %vc% GTR 9 set MSVC_TS_VER=140
+
 cmake -GNinja ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DMSVC_VERSION="%MSVC_VER%" ^
+      -DMSVC_TOOLSET_VERSION="%MSVC_TS_VER%" ^
       -DCMAKE_C_USE_RESPONSE_FILE_FOR_OBJECTS:BOOL=FALSE ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DBUILD_SHARED_LIBS=1 ^
