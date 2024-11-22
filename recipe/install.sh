99177e2ed9c783ec7349828a2549c5f1a1823301
Uwe L. Korn <uwe.korn@quantco.com>
2024-11-22 15:25:30 +0100

    Split package

--- recipe/install.sh
+++ recipe/install.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+
+set -euxo pipefail
+
 make install
 
-if [[ "$PKG_NAME" == *static ]]
-then
-	# relying on conda to dedup package
-	echo "Keeping all files, conda will dedupe"
-else
-	rm -rf ${PREFIX}/lib/*.a
+if [[ "${PKG_NAME}" != "xz-static" ]]; then
+    rm -rf ${PREFIX}/lib/*.a
+fi
+
+if [[ "${PKG_NAME}" != "xz-gpl-tools" ]]; then
+    rm ${PREFIX}/share/doc/xz/COPYING.GPLv2
+    rm ${PREFIX}/bin/{lzcat,lzcmp,lzdiff,lzegrep,lzfgrep,lzgrep,lzless,lzmore,xzcat,xzcmp,xzdiff,xzegrep,xzfgrep,xzgrep,xzless,xzmore}
+    rm ${PREFIX}/share/man/man1/{lzcat,lzcmp,lzdiff,lzegrep,lzfgrep,lzgrep,lzless,lzmore,xzcat,xzcmp,xzdiff,xzegrep,xzfgrep,xzgrep,xzless,xzmore}.1
+fi
+
+if [[ "${PKG_NAME}" != "xz-tools" ]]; then
+    rm ${PREFIX}/share/doc/xz/COPYING.0BSD
+    rm ${PREFIX}/bin/{lzma,lzmadec,lzmainfo,xz,unlzma,xzdec,unxz}
+    rm ${PREFIX}/share/man/man1/{lzma,lzmadec,lzmainfo,xz,unlzma,xzdec,unxz}.1
 fi
