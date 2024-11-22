463bb3b4cd2a04cdbcf592d5038496ab916d14b2
Uwe L. Korn <uwe.korn@quantco.com>
2024-11-22 21:00:50 +0100

    lzcat and xzcat are 0BSD

--- recipe/install.sh
+++ recipe/install.sh
@@ -10,12 +10,12 @@ fi
 
 if [[ "${PKG_NAME}" != "xz-gpl-tools" ]]; then
     rm ${PREFIX}/share/doc/xz/COPYING.GPLv2
-    rm ${PREFIX}/bin/{lzcat,lzcmp,lzdiff,lzegrep,lzfgrep,lzgrep,lzless,lzmore,xzcat,xzcmp,xzdiff,xzegrep,xzfgrep,xzgrep,xzless,xzmore}
-    rm ${PREFIX}/share/man/man1/{lzcat,lzcmp,lzdiff,lzegrep,lzfgrep,lzgrep,lzless,lzmore,xzcat,xzcmp,xzdiff,xzegrep,xzfgrep,xzgrep,xzless,xzmore}.1
+    rm ${PREFIX}/bin/{lzcmp,lzdiff,lzegrep,lzfgrep,lzgrep,lzless,lzmore,xzcmp,xzdiff,xzegrep,xzfgrep,xzgrep,xzless,xzmore}
+    rm ${PREFIX}/share/man/man1/{lzcmp,lzdiff,lzegrep,lzfgrep,lzgrep,lzless,lzmore,xzcmp,xzdiff,xzegrep,xzfgrep,xzgrep,xzless,xzmore}.1
 fi
 
 if [[ "${PKG_NAME}" != "xz-tools" ]]; then
     rm ${PREFIX}/share/doc/xz/COPYING.0BSD
-    rm ${PREFIX}/bin/{lzma,lzmadec,lzmainfo,xz,unlzma,xzdec,unxz}
-    rm ${PREFIX}/share/man/man1/{lzma,lzmadec,lzmainfo,xz,unlzma,xzdec,unxz}.1
+    rm ${PREFIX}/bin/{lzcat,lzma,lzmadec,lzmainfo,xz,xzcat,unlzma,xzdec,unxz}
+    rm ${PREFIX}/share/man/man1/{lzcat,lzma,lzmadec,lzmainfo,xz,xzcat,unlzma,xzdec,unxz}.1
 fi
