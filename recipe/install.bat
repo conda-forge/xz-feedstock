3f06d7778ca41057aac809de2f7be80a4e035177
Uwe L. Korn <uwe.korn@quantco.com>
2024-11-24 21:01:17 +0100

    Use backslashes

--- recipe/install.bat
+++ recipe/install.bat
@@ -4,8 +4,15 @@ ninja install
 if errorlevel 1 exit /b 1
 
 if %PKG_NAME% NEQ "xz-tools" (
-    del %LIBRARY_PREFIX%/bin/xzdec.exe
-    del %LIBRARY_PREFIX%/bin/lzmadec.exe
-    del %LIBRARY_PREFIX%/bin/lzmainfo.exe
-    del %LIBRARY_PREFIX%/bin/xz.exe
+    del %LIBRARY_PREFIX%\bin\xzdec.exe
+    if errorlevel 1 exit /b 1
+
+    del %LIBRARY_PREFIX%\bin\lzmadec.exe
+    if errorlevel 1 exit /b 1
+
+    del %LIBRARY_PREFIX%\bin\lzmainfo.exe
+    if errorlevel 1 exit /b 1
+
+    del %LIBRARY_PREFIX%\bin\xz.exe
+    if errorlevel 1 exit /b 1
 )
