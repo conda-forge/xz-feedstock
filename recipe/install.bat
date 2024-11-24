08e523a757a9afeb930b5bb6279239f67ee0a1c1
Uwe L. Korn <uwe.korn@quantco.com>
2024-11-24 20:44:45 +0100

    Remove bash code

--- recipe/install.bat
+++ recipe/install.bat
@@ -3,11 +3,6 @@
 ninja install
 if errorlevel 1 exit /b 1
 
-if [[ "${PKG_NAME}" != "xz-tools" ]]; then
-    rm ${PREFIX}/share/doc/xz/COPYING.0BSD
-    rm ${PREFIX}/bin/{lzcat,lzma,lzmadec,lzmainfo,xz,xzcat,unlzma,xzdec,unxz}
-    rm ${PREFIX}/share/man/man1/{lzcat,lzma,lzmadec,lzmainfo,xz,xzcat,unlzma,xzdec,unxz}.1
-fi
 if %PKG_NAME% NEQ "xz-tools" (
     del %LIBRARY_PREFIX%/bin/xzdec.exe
     del %LIBRARY_PREFIX%/bin/lzmadec.exe
